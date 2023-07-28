c1b8173ba1c79563f9defbd7643b6016f5c947dd
Danny Gilberg <danielgilberg@gmail.com>
2023-07-28 00:37:55 -0400

    updates code and adds config file

--- world_cup_code.py
+++ world_cup_code.py
@@ -208,7 +208,7 @@ def scrape_team_match_report_from_schedule(row, config):
 
     file_name = '{}_{}_{}_vs_{}_match_summary.pkl'.format(team, row['match_id'], md_str, opp)
     full_path = os.path.join(match_report_dir, file_name)
-    df = clean_match_report(df, config, row)
+    df = clean_single_team_match_report(df, config, row)
     df.to_pickle(full_path)
     return df
 
@@ -236,7 +236,7 @@ def cast_dtypes(df, datatypes):
             new_df[i] = new_df[i].astype(datatypes[i])
     return new_df
 
-def clean_match_report(df, config, row):
+def clean_single_team_match_report(df, config, row):
     """
         cleans a match report--gives better column names and casts right datatypes
 
@@ -349,3 +349,148 @@ def clean_roster(df, config):
     df = df.rename(columns=config['roster_rename_columns'])
     df['age'] = df.age.astype('Int64')
     return df
+
+def scrape_competition_schedule(url, year, competition_id, config):
+    """
+        scrapes the competition schedule from the master schedule
+
+        Args:
+            url(str): url of the master schedule pag
+            year(str): year of competition
+            config(dict): values in config file
+
+        Returns:
+            df(DataFrame): cleaned roster dataframe
+    """
+    attrs = {'id': 'sched_{}_{}_1'.format(year, competition_id)}
+    df = pd.read_html(url, attrs=attrs, extract_links='body')[0]
+    df.columns = [i.lower().replace(' ', '_') for i in df.columns]
+    link_cols = ['home', 'away', 'match_report', 'date']
+    standard_cols = [i for i in df.columns if i not in link_cols]
+    for i in standard_cols:
+        df[i] = df.apply(lambda row: row[i][0], axis=1)
+
+    for j in link_cols:
+        new_col = j + "_link"
+        df[new_col] = df.apply(lambda row: row[j][1], axis=1)
+        df[j] = df.apply(lambda row: row[j][0], axis=1)
+    df = df[df.match_report == 'Match Report']
+    df = clean_competition_schedule(df, config)
+    return df
+
+def clean_competition_schedule(df, config):
+    """
+        cleans the competition schedule and renames columns and re-assigns datatypes
+
+        Args:
+            df(DataFrame): data to be cleaned
+            config(dict): values from config file
+
+        Returns:
+            df(DataFrame): cleaned df
+
+    """
+    df = df.rename(columns=config['competition_rename_columns'])
+    df['home_team'] = df.apply(lambda row: " ".join(row['home_team'].split(' ')[:-1]), axis=1)
+    df['away_team'] = df.apply(lambda row: " ".join(row['away_team'].split(' ')[1:]), axis=1)
+    df['home_goals'] = df.apply(lambda row: row['score'].split('–')[0], axis=1)
+    df['away_goals'] = df.apply(lambda row: row['score'].split('–')[1], axis=1)
+    link_columns = [i for i in df.columns if '_link' in i]
+    for col in link_columns:
+        df[col] = df.apply(lambda row: 'https://fbref.com' + row[col], axis=1)
+    df['attendance'] = df.apply(lambda row: row['attendance'].replace(',', ''), axis=1)
+    df['match_date'] = pd.to_datetime(df.match_date).dt.date
+    df['home_team_id'] = df.apply(lambda row: row['home_team_link'].split('/')[-2], axis=1)
+    df['away_team_id'] = df.apply(lambda row: row['away_team_link'].split('/')[-2], axis=1)
+    df['match_id'] = df.apply(lambda row: row['match_report_link'].split('/')[-2], axis=1)
+    return df
+
+def scrape_match_report_from_competition_schedule(row, category, config):
+    """
+        scrapes a full match report (both teams) from the competition schedule
+
+        Args:
+            row(pd.series): row of the schedule dataframe
+            category: which category of data you're pulling, consult fbref for the available ones for the competition
+            config(dict): values of the config file
+
+        Returns:
+            final(DataFrame): full match report
+
+    """
+    url = row['match_report_link']
+    home_table_id = 'stats_{}_{}'.format( row['home_team_id'], category.lower())
+    home_df = pd.read_html(url, attrs={'id': home_table_id}, extract_links='body')[0]
+    home_df.columns = [i[0].lower().replace(' ', '_') + '_'+ i[1].lower().replace(' ', '_') if 'Unnamed' not in i[0] else i[1].lower().replace(' ', '_') for i in home_df.columns ]
+    home_df['player_link'] = home_df.apply(lambda row: row['player'][1], axis=1)
+    home_df['player'] = home_df.apply(lambda row: row['player'][0], axis=1)
+    cols = [i for i in home_df.columns if i not in ['player', 'player_link']]
+    home_df = home_df[pd.notnull(home_df['#'])]
+    for i in cols:
+        home_df[i] = home_df.apply(lambda row: row[i][0], axis=1)
+    home_df['match_id'] = row['match_id']
+    home_df['squad'] = row['home_team']
+    home_df['opponent'] = row['away_team']
+
+    away_table_id = 'stats_{}_{}'.format( row['away_team_id'], category.lower())
+    away_df = pd.read_html(url, attrs={'id': away_table_id}, extract_links='body')[0]
+    away_df.columns = [i[0].lower().replace(' ', '_') + '_'+ i[1].lower().replace(' ', '_') if 'Unnamed' not in i[0] else i[1].lower().replace(' ', '_') for i in away_df.columns ]
+    away_df['player_link'] = away_df.apply(lambda row: row['player'][1], axis=1)
+    away_df['player'] = away_df.apply(lambda row: row['player'][0], axis=1)
+    cols = [i for i in away_df.columns if i not in ['player', 'player_link']]
+    away_df = away_df[pd.notnull(away_df['#'])]
+    for i in cols:
+        away_df[i] = away_df.apply(lambda row: row[i][0], axis=1)
+    away_df['match_id'] = row['match_id']
+    away_df['squad'] = row['away_team']
+    away_df['opponent'] = row['home_team']
+
+    file_dir = 'data/womens_world_cup/world_cup_matches'
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    file_name = '{}_{}.pkl'.format(row['match_id'], category.lower())
+    full_path = os.path.join(file_dir, file_name)
+    final = pd.concat([home_df, away_df], ignore_index=True)
+    final = final.rename(columns=config['match_report_rename_columns'])
+    final.to_pickle(full_path)
+    return final
+
+def scrape_match_report_all_categories(row, config, file_path, file_check=True):
+    """
+        scrapes a match report for all categories and merges them
+
+        Args:
+            row(pd.series): row from Schedule DataFrame
+            config(dict): values of config file
+            file_path(str): path to check for existing reports
+            file_check(bool): whether or not you want to check for an existing report
+
+    """
+    file_check_arr = os.listdir(file_path)
+    file_name = '{}_report.pkl'.format(row['match_id'])
+    if file_name in file_check_arr and file_check:
+        return False
+
+    categories = ['summary', 'passing', 'passing_types', 'defense', 'misc', 'possession']
+    for j in categories:
+        try:
+            scrape_match_report_from_competition_schedule(row, j, config)
+            time.sleep(6)
+        except Exception as e:
+            print(e)
+
+    files = all_files_in_subdirectories(file_path, key_terms=row['match_id'])
+    dfs = list()
+    for i in files:
+        dfs.append(pd.read_pickle(i))
+
+    merged_df = reduce(lambda left, right: pd.merge(left, right), dfs)
+    full_report_path = 'data/womens_world_cup/world_cup_matches/'
+    if not os.path.exists(full_report_path):
+        os.makedirs(full_report_path)
+
+    file_name = '{}_report.pkl'.format(row['match_id'])
+    full_path = os.path.join(full_report_path, file_name)
+    merged_df.to_pickle(full_path)
+
+    
